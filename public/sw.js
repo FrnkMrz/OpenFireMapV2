e14d25b6b6edf0096d426f716e30118514157bbb
Frank März <github@rollhofen.de>
2026-04-02 18:45:27 +0200

    fix: invalidate app cache on mobile devices

--- public/sw.js
+++ public/sw.js
@@ -12,7 +12,7 @@
  * veraltete Wasserentnahmestellen suggerieren könnte.
  */
 
-const CACHE_NAME = 'ofm-v4-static';
+const CACHE_NAME = 'ofm-v5-static';
 // Nur wirklich statische Assets precachen (keine gehashten Bundles!)
 const ASSETS = [
     '/',
