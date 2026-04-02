e6a1c5d4495fad38d0a62acef4a4750fa1755ab6
Frank März <github@rollhofen.de>
2026-04-02 19:07:28 +0200

    chore: bump cache for service worker

--- public/sw.js
+++ public/sw.js
@@ -12,7 +12,7 @@
  * veraltete Wasserentnahmestellen suggerieren könnte.
  */
 
-const CACHE_NAME = 'ofm-v6-static';
+const CACHE_NAME = 'ofm-v7-static';
 // Nur wirklich statische Assets precachen (keine gehashten Bundles!)
 const ASSETS = [
     '/',
